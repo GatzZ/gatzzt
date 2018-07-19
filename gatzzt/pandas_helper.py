ec33a57aa5ab675aa46b9e8ad071d5d25f4fe2dc
GatZz <instituteliew@gmail.com>
2018-07-19 22:02:20 +0800

    update

--- gatzzt/pandas_helper.py
+++ gatzzt/pandas_helper.py
@@ -1,4 +1,27 @@
 
+import numpy as np
+
+
 def diff_unique(x, y):
     x, y = set(x), set(y)
     return x - y, y - x
+
+
+
+def downcast_dtypes(df):
+    '''
+        Changes column types in the dataframe: 
+                
+                `float64` type to `float32`
+                `int64`   type to `int32`
+    '''
+    
+    # Select columns to downcast
+    float_cols = [c for c in df if df[c].dtype == "float64"]
+    int_cols =   [c for c in df if df[c].dtype == "int64"]
+    
+    # Downcast
+    df[float_cols] = df[float_cols].astype(np.float32)
+    df[int_cols]   = df[int_cols].astype(np.int32)
+    
+    return df
